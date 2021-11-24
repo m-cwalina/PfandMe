f951a7371b6b37f11ce4481775710cb8e8261094
Noemie Brancourt <noemie_brancourt@hotmail.com>
2021-11-24 16:46:25 +0100

    displaying offers open to pick up in the index

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers do
-    resources :bookings
+    resources :bookings, only: [:new, :create]
   end
 
   get '/dashboard', to: 'pages#dashboard', as: :dashboard
+
+  resources :bookings, only: [:index, :show, :edit, :update]
 end
