2a5194fd64a925e3adf0f629702064a75cb675ed
Noemie Brancourt <noemie_brancourt@hotmail.com>
2021-11-24 11:26:28 +0100

    offers new route fixed, showing show page

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -15,7 +15,7 @@ class OffersController < ApplicationController
     @offer = Offer.new(offer_params)
     @offer.user = current_user
     if @offer.save
-      redirect_to offers_path, notice: 'Offer was successfully created.'
+      redirect_to offer_path(@offer), notice: 'Offer was successfully created.'
     else
       render :new
     end
