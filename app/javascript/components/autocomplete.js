c2c487c15649ef9bd7c7e7491c32924c57de89e7
Noemie Brancourt <noemie_brancourt@hotmail.com>
2021-11-30 15:00:44 +0100

    Map search zooming

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,16 +1,5 @@
-function autocomplete() {
-  document.addEventListener("DOMContentLoaded", function () {
-    var userAddress = document.getElementById('searchfield');
+// function autocomplete() {
 
-    if (userAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(userAddress, { types: ['geocode'] });
-      google.maps.event.addDomListener(userAddress, 'keydown', function (e) {
-        if (e.key === "Enter") {
-          e.preventDefault(); // Do not submit the form on Enter.
-        }
-      });
-    }
-  });
-}
+// }
 
-export { autocomplete };
+// export { autocomplete };
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,14 +2,18 @@ import GMaps from 'gmaps/gmaps.js';
 import { autocomplete } from 'components/autocomplete';
 
 const mapElement = document.getElementById('map');
+var map;
+
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+
   const markers = JSON.parse(mapElement.dataset.markers).map( m =>
     {
     m["icon"] = "assets/map_icons/bottle.png";
       return m;
     }
     );
+
   map.addMarkers(markers);
   if (markers.length === 0) {
     map.setZoom(2);
@@ -19,6 +23,7 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   } else {
     map.fitLatLngBounds(markers);
   }
+
   const styles = [
     {
       "featureType": "administrative",
@@ -365,4 +370,21 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   map.setStyle('map_style');
 }
 
-  autocomplete();
+document.addEventListener("DOMContentLoaded", function () {
+  var userAddress = document.getElementById('searchfield');
+
+  if (userAddress) {
+    var autocomplete = new google.maps.places.Autocomplete(userAddress, { types: ['geocode'] });
+
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+
+      if (place.geometry) {
+        map.map.setCenter(new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng()));
+
+        // TODO: Filter Pins to those in this area only!
+        return;
+      }
+    });
+  }
+});
