a8d703b7ded15729a3fb8a7a8464b076ee8bc0bc
Benedikt von Trotha <benedikt.trotha@web.de>
2021-12-01 09:59:06 +0100

    fixed seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  belongs_to :user, dependent: :destroy
-  belongs_to :offer, dependent: :destroy
+  belongs_to :user
+  belongs_to :offer
   enum status: [:in_progress, :accepted, :completed]
 end
