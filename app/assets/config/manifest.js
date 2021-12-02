4177e334e908e9a2c63f3b54ddc059e4893615cd
Matthew Cwalina <matthewcwalina@gmail.com>
2021-12-02 17:52:36 +0100

    Added A2HA

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
+// app/assets/config/manifest.json.erb
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+// app/assets/config/manifest.js
+// ...
+//= link manifest.json
+//= link cover.png
